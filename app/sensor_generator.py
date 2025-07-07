9d64f56b7acfbb1dc9a4ec2e4211c94cf463375c
nishad1092 <nishad1092@email.com>
2025-07-07 14:19:51 +0200

    restructuring the sensor generatro

--- app/sensor_generator.py
+++ app/sensor_generator.py
@@ -1,14 +1,1 @@
-# app/main.py
-from flask import Flask, jsonify
-import pandas as pd
-
-app = Flask(__name__)
-
-@app.route("/data", methods=["GET"])
-def get_sensor_data():
-    df = pd.read_csv("./sensor-data.csv")
-    data = df.tail(50).to_dict(orient="records")  # Last 50 records
-    return jsonify(data)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+import 
